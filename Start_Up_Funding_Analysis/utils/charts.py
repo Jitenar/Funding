fd7fdd00329e4c6c31c7c16f9bc96ee4e5ed2a95
Jitenar <jitendar.deshna@gmail.com>
2026-06-04 15:09:38 +0530

    Update charts.py

--- Start_Up_Funding_Analysis/utils/charts.py
+++ Start_Up_Funding_Analysis/utils/charts.py
@@ -1,1 +1,44 @@
+import plotly.express as px
 
+
+def line_chart(data, x, y, title):
+    fig = px.line(
+        data,
+        x=x,
+        y=y,
+        markers=True,
+        title=title
+    )
+    return fig
+
+
+def bar_chart(data, x, y, title):
+    fig = px.bar(
+        data,
+        x=x,
+        y=y,
+        title=title
+    )
+    return fig
+
+
+def horizontal_bar_chart(data, x, y, title):
+    fig = px.bar(
+        data,
+        x=x,
+        y=y,
+        orientation="h",
+        title=title
+    )
+    return fig
+
+
+def pie_chart(data, names, values, title):
+    fig = px.pie(
+        data,
+        names=names,
+        values=values,
+        hole=0.4,
+        title=title
+    )
+    return fig
